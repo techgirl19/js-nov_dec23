c82934f8aebd4a17e0e72c4b5de2b464f554d270
techgirl19 <150898615+techgirl19@users.noreply.github.com>
2023-11-15 10:10:46 +0000

    container config done

---
+++ .devcontainer/01_basics/01variables.js
@@ -0,0 +1,9 @@
+const account_id=456284
+let account_email="pp@gmail.com"
+var account_password="34838"
+account_city="punjab"
+
+account_email="ooyh@gmail.com"
+account_password="475834"
+account_city="haryana"
+console.table([account_id,account_email,account_password,account_city])
